ceba774524fc3d7b686e52310257ac7cd20c8a67
TobiasAndren <tobiasandren@hotmail.com>
2024-12-06 11:08:06 +0100

    made a somewhat working lyric api fetcher that display lyrics from submitted song with some styilng

---
+++ assets/scripts/lyrics.js
@@ -0,0 +1,42 @@
+addEventListener("submit", (event) => {  
+    const artist = document.getElementById("artist")
+    const artistValue = artist.value.toUpperCase()
+
+    const title = document.getElementById("title")
+    const titleValue = title.value.toUpperCase()
+
+    let url = `https://api.lyrics.ovh/v1/${artistValue}/${titleValue}`
+
+    const form = document.querySelector("form")
+    form.classList.add("notVisible")
+    form.classList.toggle("lyricForm")
+
+    const goBack = document.querySelector(".goBack")
+    goBack.classList.toggle("notVisible")
+    goBack.classList.toggle("goBackContainer")
+
+    fetch(url)
+    .then((Response) => {
+        return Response.json();
+    })
+    .then((json) => {
+        if(json.lyrics){
+
+            const div = document.createElement("div")
+            div.innerHTML = `
+            <h1 class="artistText">${artistValue}</h1>
+            <h3 class="titleText">${titleValue}</h3>
+            <pre class="lyricText">${json.lyrics}</pre>`
+            
+            document.body.appendChild(div)
+            div.classList.add("lyricContainer")
+        } else {
+            const div = document.createElement("div")
+            div.innerHTML = `
+            <h2>Sorry! song not found. Try Again!</h2>
+            `
+            document.body.appendChild(div)
+            div.classList.add("lyricContainer")
+        }
+    })
+})
